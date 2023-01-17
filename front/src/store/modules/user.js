bbaa1f7bfa64376bf258917b09979f7f9b70c85d
Nouira Alim <nouiraalim@gmail.com>
2023-01-17 14:29:03 +0100

    Client store

---
+++ front/src/store/modules/user.js
@@ -0,0 +1,37 @@
+// import helpers from '@/helpers'
+import reqHand from '../fetch'
+
+// initial state
+const state = {
+}
+
+// getters
+const getters = {
+}
+
+// actions
+const actions = {
+  async loginUser({ state, commit }, newClient) {
+    return reqHand.post('/clients/login', body = newClient)
+    .then((response) => {
+      response.json()
+      .then((data) => {
+        commit('RECEIVE_USER', { data })
+      })
+    })
+  },
+}
+
+// mutations
+const mutations = {
+  ['RECEIVE_USER'](state, data) {
+    localStorage.setItem("accessToken", data.accessToken)
+  },
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
