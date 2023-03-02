57e5768fb6eee15f0a26eb12f14ce3fde76a9e5b
arnaudmaturel <73825573+arnaudmaturel@users.noreply.github.com>
2023-03-02 17:05:15 +0100

    Update Front
    - CGV
    - About (photos, ...)
    - Confidentiality (RGPD, ...)

--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,7 +1,119 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-row no-gutters>
+    <v-col cols="12" style="background-color: grey" align-self="center">
+      <v-card style="background-color: #EEEEEE; padding-top: 2%; padding-bottom: 2%; padding-left: 8%; padding-right: 8%;" min-height="400">
+        <v-card color="white" style="border-radius: 40px;">
+          <v-row align="center">
+            <v-col cols="12">
+              <v-card-title style="color: orange; font-size: 44px; text-align: justify; margin-top: 20px; margin-left: 10px">
+                <b>Qui sommes nous ?</b>
+                <br><br><br>
+              </v-card-title>
+              <v-card-text style="font-size: 24px; line-height:35px; margin: 10px">
+                <b>Bienvenue sur LinkEats!, l'application de commande de repas en ligne qui vous permet de rencontrer des personnes partageant les mêmes goûts culinaires que vous. Fondée en 2023 par Arnaud Maturel, Antoine Legois, Alim Nouira et Antoine Fourcin, notre objectif est de créer des liens entre les gens autour de la nourriture.
+                  <br><br>
+                  Lorsque vous commandez un plat sur LinkEats, grâce à notre Linker, vous recevez une notification vous informant que quelqu'un d'autre a commandé le même plat que vous. Vous avez alors la possibilité de contacter cette personne et de vous proposer de partager le repas ensemble. Qui sait, cela pourrait même mener à une histoire d'amour !
+                  <br><br>
+                  Chez LinkEats!, nous croyons que la nourriture est un langage universel qui peut rassembler les gens. Nous sommes fiers de proposer une plateforme qui permet aux utilisateurs de se connecter et de partager leur passion pour la cuisine.
+                  <br><br>
+                  Alors, pourquoi ne pas essayer LinkEats! pour votre prochain repas? Commandez votre plat préféré et laissez l'application vous connecter à d'autres amateurs de cuisine pour une expérience culinaire unique. N'oubliez pas notre slogan : "Lick It!"
+                </b>
+              </v-card-text>
+              <v-row>
+                <v-col cols="3">
+                  <v-card-item>
+                    <v-col>
+                      <v-row>
+                        <v-img width="100%" max-height="330" src="../assets/Antoine.jpg"></v-img>
+                      </v-row>
+                      <v-row>
+                        <v-card-text align="center">
+                          Antoine FOURCIN, Chef de projet
+                        </v-card-text>
+                      </v-row>
+                    </v-col>
+                  </v-card-item>
+                </v-col>
+                <v-col cols="3">
+                  <v-card-item>
+                    <v-col>
+                      <v-row>
+                        <v-img width="100%" max-height="330" src="../assets/AntoineLegois.png"></v-img>
+                      </v-row>
+                      <v-row>
+                        <v-card-text align="center">
+                          Antoine LEGOIS, Développeur
+                        </v-card-text>
+                      </v-row>
+                    </v-col>
+                  </v-card-item>
+                </v-col>
+                <v-col cols="3">
+                  <v-card-item>
+                    <v-col>
+                      <v-row>
+                        <v-img width="100%" max-height="330" src="../assets/Arnaud.jpg"></v-img>
+                      </v-row>
+                      <v-row>
+                        <v-card-text align="center">
+                          Arnaud MATUREL, Développeur
+                        </v-card-text>
+                      </v-row>
+                    </v-col>
+                  </v-card-item>
+                </v-col>
+                <v-col cols="3">
+                  <v-card-item>
+                    <v-col>
+                      <v-row>
+                        <v-img width="100%" max-height="330" src="../assets/alim.jpg"></v-img>
+                      </v-row>
+                      <v-row>
+                        <v-card-text align="center">
+                          Alim NOUIRA, DevOps
+                        </v-card-text>
+                      </v-row>
+                    </v-col>
+                  </v-card-item>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row no-gutters>
+    <v-col cols="12" style="background-color: #27476E" align-self="center">
+      <v-card style="background-color: #02678f; padding-top: 2%;" min-height="300">
+        <v-row align="center">
+          <v-col cols="2"></v-col>
+          <v-col align-self="center" cols="8">
+            <v-card-title style="color: orange; font-size: 44px; text-align: justify; margin-top: 20px; margin-left: 10px">
+              <b>Pour nous contacter</b>
+              <br><br><br>
+            </v-card-title>
+            <v-card-text style="font-size: 24px; line-height:35px; margin: 10px; color: white">
+              Nous sommes ravis de vous aider avec toutes vos questions et vos commentaires. Voici comment nous contacter :
+              <br><br>
+              Service clientèle : Si vous avez des questions sur votre commande, votre compte ou toute autre question liée à notre service, veuillez envoyer un e-mail à notre service clientèle à l'adresse suivante : support@linkeats.com. Nous nous engageons à vous répondre dans les plus brefs délais.
+              <br><br>
+              Assistance technique : Si vous rencontrez des problèmes techniques lors de l'utilisation de notre application, veuillez envoyer un e-mail à notre équipe d'assistance technique à l'adresse suivante : techsupport@linkeats.com. Notre équipe s'efforcera de résoudre votre problème dans les meilleurs délais.
+              <br><br>
+              Partenariats : Si vous êtes intéressé par un partenariat avec LinkEats!, veuillez envoyer un e-mail à notre équipe de partenariats à l'adresse suivante : partnerships@linkeats.com. Nous sommes toujours à la recherche de nouvelles opportunités de partenariat pour améliorer notre service et offrir une expérience encore plus riche à nos utilisateurs.
+              <br><br>
+              Presse : Si vous êtes journaliste et que vous souhaitez en savoir plus sur notre entreprise et notre service, veuillez envoyer un e-mail à notre équipe de presse à l'adresse suivante : press@linkeats.com. Nous serons heureux de répondre à toutes vos questions et de vous fournir toutes les informations dont vous avez besoin.
+              <br><br>
+              Nous sommes toujours heureux d'entendre vos commentaires et vos suggestions. Si vous avez des idées pour améliorer notre service, veuillez envoyer un e-mail à feedback@linkeats.com. Nous sommes impatients de travailler avec vous pour améliorer notre service et offrir la meilleure expérience possible à nos utilisateurs.
+              <br><br>
+              Merci d'avoir choisi LinkEats!. Nous sommes là pour vous aider à tout moment et nous nous engageons à fournir le meilleur service possible.
+            </v-card-text>
+          </v-col>
+          <v-col cols="2"></v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
